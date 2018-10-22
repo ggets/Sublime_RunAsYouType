72c85437dc273592ef33fb785af5842b67afdce6
hkrware <4815620+hkrware@users.noreply.github.com>
2018-10-22 16:28:01 +0300

    Update 1
    Updated the README to shed some light on the settings file and added the calculation script.

---
+++ scripts/php/calc.php
@@ -0,0 +1,12 @@
+<?php
+$c="";
+while(!feof(STDIN))$c.=fgets(STDIN);
+$a=explode(";",$c);
+foreach($a as $v){
+  if($v){
+    $v=preg_replace('/\s+/','',$v);
+    if(!strlen($v))echo $v;
+    else eval("echo ({$v});");
+    echo (';'.PHP_EOL);
+  }
+}
